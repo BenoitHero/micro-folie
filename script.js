4d65829be1f4a7c856d053bae8e3e14e18c2b1de
BenoitHero <bhero@grandverdun.fr>
2025-03-19 14:45:36 +0100

    tfdx

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const GAME_COLLECTION_NAME = 'application_micro-folie';
  * Etape 2
  * Editer la taille du jeu
  */
-const LARGEUR_DU_JEU = 2500;
-const HAUTEUR_DU_JEU = 1000;
+const LARGEUR_DU_JEU = 3000;
+const HAUTEUR_DU_JEU = 1500;
 /**
  * Etape 3
  * Editer la configuration de Firebase si besoin
